d12d41c4d81d564bfa734285e6af951dd7ef7222
baohc1705 <devblue404@gmail.com>
2026-05-08 14:34:33 +0700

    init be

---
+++ src/mini-management-system-be/src/main/java/com/baohc/minimanagementsystembe/MiniManagementSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.baohc.minimanagementsystembe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniManagementSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniManagementSystemBeApplication.class, args);
+    }
+
+}
---
+++ src/mini-management-system-be/src/test/java/com/baohc/minimanagementsystembe/MiniManagementSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baohc.minimanagementsystembe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniManagementSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
